e27ec7eefc5691e9b444e353ba5343c6cde9e551
Ari <88184960+Templight41@users.noreply.github.com>
2023-11-07 11:40:48 +0000

    few bug fix

--- app.js
+++ app.js
@@ -25,59 +25,133 @@ for(button of buttons) {
         }
         else {
             // console.log("operation detected")
-            if(value=="C") {
-                nums = [0];
-                prevValue = 0;
-                currentValue = "";
-                screen.innerText = currentValue
+            switch (value) {
+                case "C":
+                    nums = [0];
+                    prevValue = 0;
+                    currentValue = "";
+                    finalValue = 0;
+                    currentValueFloat = 0;
+                    screen.innerText = currentValue
+                    break;
+
+                case "plus":
+                    operation = "+";
+                    currentValue = "";
+                    prevValue = currentValueFloat + prevValue
+                    screen.innerText = currentValueFloat
+                    break;
+
+                case "minus":
+                    operation = "-";
+                    currentValue = "";
+                    prevValue = currentValueFloat - prevValue;
+                    break;
+
+                case "multiply":
+                    operation = "*";
+                    prevValue = currentValueFloat;
+                    currentValue = "";
+                    break;
+
+                case "divide":
+                    operation = "/";
+                    prevValue = currentValueFloat;
+                    currentValue = "";
+                    break;
+                
+                case "percentage":
+                    operation = "%";
+                    prevValue = currentValueFloat/100;
+                    currentValue = "";
+                    break;
+
+                case "plusMinus":
+                    operation = "+-";
+                    prevValue = -(currentValueFloat);
+                    currentValue = "";
+                    break;
+
+                case "equal":
+                    switch (operation) {
+                        case "+":
+                            prevValue = prevValue + currentValueFloat
+                            break;
+
+                        case "-":
+                            prevValue = prevValue - currentValueFloat
+                            break;
+
+                        case "*":
+                            prevValue = prevValue * currentValueFloat
+                            break;
+
+                        case "/":
+                            prevValue = prevValue / currentValueFloat
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+                
+
+                default:
+                    break;
             }
-            else if (value=="plus") {
-                operation = "+";
-                currentValue = "";
-                prevValue = currentValueFloat + prevValue
-                screen.innerText = currentValueFloat
-                // prevValue = currentValueFloat;
-            }
-            else if(value == "minus") {
-                operation = "-";
-                currentValue = "";
-                prevValue = currentValueFloat - prevValue;
-            }
-            else if(value=="multiply") {
-                operation = "*";
-                prevValue = currentValueFloat;
-                currentValue = "";
-            }
-            else if(value == "divide") {
-                operation = "/";
-                prevValue = currentValueFloat;
-                currentValue = "";
-            }
-            else if(value == "percentage") {
-                operation = "%";
-                prevValue = currentValueFloat/100;
-                currentValue = "";
-            }
-            else if(value == "plusMinus") {
-                operation = "+-";
-                prevValue = -(currentValueFloat);
-                currentValue = "";
-            }
-            else if(value=="equal") {
-                console.log(currentValue);
-                if(operation=="+") {
-                    prevValue = prevValue + currentValueFloat
-                }
-                else if(operation=="-") {
-                    prevValue = prevValue - currentValueFloat
-                }
-                else if(operation=="*") {
-                    prevValue = prevValue * currentValueFloat
-                }
-                else if(operation=="/") {
-                    prevValue = prevValue / currentValueFloat
-                }
-            }
+            // if(value=="C") {
+            //     nums = [0];
+            //     prevValue = 0;
+            //     currentValue = "";
+            //     screen.innerText = currentValue
+            // }
+            // else if (value=="plus") {
+            //     operation = "+";
+            //     currentValue = "";
+            //     prevValue = currentValueFloat + prevValue
+            //     screen.innerText = currentValueFloat
+            //     // prevValue = currentValueFloat;
+            // }
+            // else if(value == "minus") {
+            //     operation = "-";
+            //     currentValue = "";
+            //     prevValue = currentValueFloat - prevValue;
+            // }
+            // else if(value=="multiply") {
+            //     operation = "*";
+            //     prevValue = currentValueFloat;
+            //     currentValue = "";
+            // }
+            // else if(value == "divide") {
+            //     operation = "/";
+            //     prevValue = currentValueFloat;
+            //     currentValue = "";
+            // }
+            // else if(value == "percentage") {
+            //     operation = "%";
+            //     prevValue = currentValueFloat/100;
+            //     currentValue = "";
+            // }
+            // else if(value == "plusMinus") {
+            //     operation = "+-";
+            //     prevValue = -(currentValueFloat);
+            //     currentValue = "";
+            // }
+            // else if(value=="equal") {
+            //     console.log(currentValue);
+            //     if(operation=="+") {
+            //         prevValue = prevValue + currentValueFloat
+            //     }
+            //     else if(operation=="-") {
+            //         prevValue = prevValue - currentValueFloat
+            //     }
+            //     else if(operation=="*") {
+            //         prevValue = prevValue * currentValueFloat
+            //     }
+            //     else if(operation=="/") {
+            //         prevValue = prevValue / currentValueFloat
+            //     }
+            // }
             currentValueStr = prevValue + ""
             screen.innerText = currentValueStr.slice(0,15)
         }
